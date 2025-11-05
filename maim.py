7620f5655e769b12de4facb4a9d8af1ba5db9a9d
bongsun08 <uaya2609@gmail.com>
2025-11-05 09:22:20 +0900

    Update maim.py

--- maim.py
+++ maim.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
 a=st.text_input('이름이 머누')
-st.selectbox('좋아하는 음식을 골라바',['마라탕', '떡볶이', '김치찌개'])
+b=st.selectbox('좋아하는 음식을 골라바',['마라탕', '떡볶이', '김치찌개'])
 if st.button('인사말 생성'):
   st.write(a+'님, 하이! 반가와')
+st.write(b+'를 조아하는 구나')
